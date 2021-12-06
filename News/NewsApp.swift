3b8311d910401e2f46448a34a86037eb086f50dd
Gabriel Frederik Brodersen <brodersen@me.com>
2021-12-06 09:17:40 +0100

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Gabriel Brodersen on 06/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
