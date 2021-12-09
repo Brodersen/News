50a42a8670acad1890f319ce6455e75e33b255a5
Gabriel Frederik Brodersen <brodersen@me.com>
2021-12-09 14:33:04 +0100

    NewsAPI Challenge Done!

---
+++ News/Authentication.swift
@@ -0,0 +1,19 @@
+//
+//  APIKey.swift
+//  News
+//
+//  Created by Gabriel Brodersen on 07/12/2021.
+//
+
+import Foundation
+
+/// Class responsible for getting an API Key
+class Authentication {
+
+	// FIXME: Exposed API Key!
+	#warning("Exposed API key for demo purposes only!")
+	static let apiKey = "70c09b9d5b964edc85f20fbdcb23aae1"
+	
+	
+}
+
